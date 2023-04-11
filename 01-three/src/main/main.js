5a8b1506300a89bb2631cdd0d3676249b00a7cd6
qijiajun <1481700559@qq.com>
2023-04-11 23:22:50 +0800

    gasp动画库学习

--- 01-three/src/main/main.js
+++ 01-three/src/main/main.js
@@ -3,6 +3,9 @@ import * as THREE from 'THREE';
 // 导入轨道控制器
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+// 导入动画库
+import gsap from 'gsap'
+
 // 目标，掌握 gsap 实现动画
 
 // 创建场景
@@ -58,16 +61,44 @@ scene.add(axesHelper)
 const clock = new THREE.Clock()
 
 
+// 设置动画,
+var animate1 = gsap.to(cube.position, {
+  x: 5,
+  duration: 5,
+  ease: 'power1.inOut',
+  // 设置重复次数，无限次为-1
+  repeat: -1,
+  // 往返运动
+  yoyo: true,
+  // 设置延迟时间
+  delay: 2,
+  onComplete: () => {
+    console.log('动画完成了')
+  },
+  onStart: () => {
+    console.log('动画开始了')
+  }
+})
+gsap.to(cube.rotation, {
+  x: 2 * Math.PI,
+  duration: 5,
+  ease: 'power1.inOut',
+})
+
+
+window.addEventListener('dblclick', () => {
+  // console.log('animate1', animate1)
+  // 暂停 恢复
+  if (animate1.isActive()) {
+    animate1.pause()
+  } else {
+    animate1.resume()
+  }
+})
+
 // 设置渲染函数
 function render () {
-  // 获取时钟运行的总时长
-  let time = clock.getElapsedTime()
-  // let deltaTime = clock.getDelta()
 
-  // console.log('时钟运行的总时长', time)
-  // console.log('两次获取时间的间隔时间', deltaTime)
-  let space = time % 5
-  cube.position.x = space
   // 使用渲染器通过相机将场景渲染出来
   renderer.render(scene, camera)
 
